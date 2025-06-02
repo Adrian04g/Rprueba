6a02cabbe6bdd0608808d669fb1ccc16bb41ccac
adrian04g <adriangomezcortes04@gmail.com>
2025-06-02 14:23:39 -0500

    Cambio general

--- Desarrollo web/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-const button = document.getElementById('cambioTexto');
-button.addEventListener('click', function(){
-    const introSection = document.getElementById('intro');
-introSection.innerHTML='<p> El texto ha cambiado¡¡</p>';
-});
-
-});
