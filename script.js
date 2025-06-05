307e691b34a8191ad47d20bb667f8919bb6c652e
adrian04g <adriangomezcortes04@gmail.com>
2025-06-05 11:01:03 -0500

    pagina personal

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ introSection.innerHTML='<p> El texto ha cambiado¡¡</p>';
 
 });
 
+function bienvenida(){
+    alert("BIenvenido a mi agina web")
+}
+
+const form = document.getElementById('contactForm');
+form.addEventListener('submit', function(event) {
+ const name = document.getElementById('name').value;
+ const email = document.getElementById('email').value;
+ if (name === '' || email === '') {
+ alert('Por favor, completa todos los campos.');
+ event.preventDefault();
+ } else {
+ alert('¡Formulario enviado correctamente!');
+ }
+});
